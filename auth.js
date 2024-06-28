4141a09fcd92cb61a41e113ae75fcffac44cf118
Yareli Cerda <yarelic.1002@gmail.com>
2024-06-28 14:28:33 -0400

    http s

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@
 
 "use strict";
 
-const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
+const apiBaseURL = "https://microbloglite.us-east-2.elasticbeanstalk.com";
 // Backup server (mirror):   "https://microbloglite.onrender.com"
 
 // NOTE: API documentation is available at /docs 
